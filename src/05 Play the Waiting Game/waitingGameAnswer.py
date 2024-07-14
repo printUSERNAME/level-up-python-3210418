92fd0cef4ae28aecf09674dfc973f6520e15075a
Aidan Abregov <aidan@abregov.com>
2024-07-14 20:41:42 +0000

    commit waiting game answer, it's not working

--- src/05 Play the Waiting Game/waitingGameAnswer.py
+++ src/05 Play the Waiting Game/waitingGameAnswer.py
@@ -1,14 +1,25 @@
 import time as t
+import random as r
 def waiting_game():
   #define your variables
-  target = 7 #seconds, get close to guessing this number of seconds
-  print("Your target time is " + target + " seconds. Good luck!")
-  print("Press ENTER to begin!")
-  #once ENTER is pressed, timer begins, timer stops after ENTER is pressed again
-  #make a conditional where closer and further guesses ellicit different responses (print)
-  #we can use the sleep funciton
-  t.sleep(1)  #program sleeps for 1 second
+  target = r.randint(2,4) #target number of seconds is randomly chosen each time
 
+  #tell the user what is going on
+  print(f'Your target time is {target} seconds.\n')
+
+  input("Press ENTER to begin!\n") #start counting here
+  start = t.perf_counter()
+
+  input(f'Press ENTER after {target} seconds...\n') #take the difference between ENTERs
+  elapsed = t.perf_counter() - start
+
+  print(f'Elapsed time: {elapsed :.3f}') #round to the nearest millisecond
+  if target == elapsed:
+    print("Unbelieveable!!! Right on the money!!!$$$")
+  elif target < elapsed:
+    print(f'You were {elapsed-target :.3f} seconds too slow. Try being faster next time!')
+  else:
+    print(f'You were {target-elapsed :.3f} seconds too fast! Slow down buddy!')
 
 
 
